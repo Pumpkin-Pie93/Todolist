63cb03e45f1af48119e16699983adc38c8d5b19e
Polina Khmialnitskaya <polinagreenheart@gmail.com>
2023-09-12 09:45:50 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todoolist";
+
+function App() {
+    return (
+        <div className="App">
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+
+        </div>
+    );
+}
+
+export default App;
+
